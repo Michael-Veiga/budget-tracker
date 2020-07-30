7a6f95061d7a3a18cba2268d06eaadacccd16abe
Mike V <mvee333@gmail.com>
2020-07-30 10:44:21 -0400

    created db.js for offline

---
+++ public/db.js
@@ -0,0 +1,54 @@
+let db;
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  db.createObjectStore('pending', { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+  db = event.target.result;
+  // check to see if the app is online before accessing the db.js
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function (event) {
+  console.log(event.target.errorCode);
+};
+
+function saveRecord(record) {
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const store = transaction.objectStore('pending');
+
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(['pending'], 'readwrite');
+  const store = transaction.objectStore('pending');
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json',
+        },
+      })
+        .then(response => response.json())
+        .then(() => {
+          const transaction = db.transaction(['pending'], 'readwrite');
+          const store = transaction.objectStore('pending');
+          store.clear();
+        });
+    }
+  };
+}
+
+// event listener for app coming back online
+window.addEventListener('online', checkDatabase);
